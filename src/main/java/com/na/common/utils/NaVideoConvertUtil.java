011c969582f22677c22c8077c5243007b5916ecc
na <na_tech@163.com>
2025-07-21 08:59:37 +0800

    init

--- src/main/java/com/na/common/utils/NaVideoConvertUtil.java
+++ src/main/java/com/na/common/utils/NaVideoConvertUtil.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-public class VideoConvertUtil {
+public class NaVideoConvertUtil {
 
     /**
      * MP4转M3U8
